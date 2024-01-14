8af8b6ce95cce6c1c925c4a6e67d3f854aa4ad06
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-01-15 00:53:52 +0900

    Lesson4-最小限のアプリを作成しよう

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -10,6 +10,6 @@ public class PostController {
     public String showHello(Model model){  //返り値が文字列（String）のメソッド、Modelオブジェクトの受け取り
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);   //addAttributeでmodelにデータを追加している、1つ目のsampleTextはビューで表示する際の名前、2つ目の名前は11行目で指定しているsampleText
-        return "hello"; //htmlファイルの指定。.html部分は省略可能
+        return "hello"; //htmlファイルの指定。.htmlの部分は省略可能
     }
 }
