29e4cf98984fefde4fb1774eee345c15e234d301
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-01-15 00:47:28 +0900

    Lesson4-最小限のアプリを作成しよう

---
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -0,0 +1,15 @@
+package in.techcamp.firstapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //そのクラスがコントローラーであることを伝えるアノテーション
+public class PostController {
+    @GetMapping("/hello")   //ブラウザで入力されたURLと実行されるメソッドを紐付けるためのアノテーション
+    public String showHello(Model model){  //返り値が文字列（String）のメソッド、Modelオブジェクトの受け取り
+        var sampleText = "サンプルテキスト";
+        model.addAttribute("sampleText", sampleText);   //addAttributeでmodelにデータを追加している、1つ目のsampleTextはビューで表示する際の名前、2つ目の名前は11行目で指定しているsampleText
+        return "hello"; //htmlファイルの指定。.html部分は省略可能
+    }
+}
