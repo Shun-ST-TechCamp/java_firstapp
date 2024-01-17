7f62e203141ce2c4b41cec02de3dfb211106197b
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-01-18 03:27:19 +0900

    一覧表示機能を実装しようの「データベースからデータを取得しよう」のエラー

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,15 +1,26 @@
 package in.techcamp.firstapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller //そのクラスがコントローラーであることを伝えるアノテーション
+@Controller
+@RequiredArgsConstructor
 public class PostController {
-    @GetMapping("/hello")   //ブラウザで入力されたURLと実行されるメソッドを紐付けるためのアノテーション
-    public String showHello(Model model){  //返り値が文字列（String）のメソッド、Modelオブジェクトの受け取り
+    private final PostRepository postRepository;
+
+    @GetMapping("/hello")
+    public String showHello(Model model){
         var sampleText = "サンプルテキスト";
-        model.addAttribute("sampleText", sampleText);   //addAttributeでmodelにデータを追加している、1つ目のsampleTextはビューで表示する際の名前、2つ目の名前は11行目で指定しているsampleText
-        return "hello"; //htmlファイルの指定。.htmlの部分は省略可能
+        model.addAttribute("sampleText", sampleText);
+        return "hello";
+    }
+
+    @GetMapping
+    public String showList(Model model){
+        var postList = postRepository.findAll();
+        model.addAttribute("postList", postList);
+        return "index";
     }
 }
