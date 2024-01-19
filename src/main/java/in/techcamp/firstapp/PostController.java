eccce07d7c045750a5de854d5da2223e43970be7
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-01-20 00:43:02 +0900

    一覧表示機能を実装しようのエラーが出る前に戻す

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,26 +1,20 @@
 package in.techcamp.firstapp;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-@RequiredArgsConstructor
+@Controller //そのクラスがコントローラーであることを伝えるアノテーション
 public class PostController {
-    private final PostRepository postRepository;
-
-    @GetMapping("/hello")
-    public String showHello(Model model){
+    @GetMapping("/hello")   //ブラウザで入力されたURLと実行されるメソッドを紐付けるためのアノテーション
+    public String showHello(Model model){  //返り値が文字列（String）のメソッド、Modelオブジェクトの受け取り
         var sampleText = "サンプルテキスト";
-        model.addAttribute("sampleText", sampleText);
-        return "hello";
+        model.addAttribute("sampleText", sampleText);   //addAttributeでmodelにデータを追加している、1つ目のsampleTextはビューで表示する際の名前、2つ目の名前は11行目で指定しているsampleText
+        return "hello"; //htmlファイルの指定。.html部分は省略可能
     }
 
     @GetMapping
-    public String showList(Model model){
-        var postList = postRepository.findAll();
-        model.addAttribute("postList", postList);
+    public String showList(){
         return "index";
     }
 }
