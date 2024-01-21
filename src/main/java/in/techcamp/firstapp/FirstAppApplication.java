cc15cd8fb565318c4f987d83341ff8b2800b05bd
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-01-21 16:01:19 +0900

    spring-boot-starterのバージョンをあげることにより解決

--- src/main/java/in/techcamp/firstapp/FirstAppApplication.java
+++ src/main/java/in/techcamp/firstapp/FirstAppApplication.java
@@ -1,9 +1,11 @@
 package in.techcamp.firstapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("in.techcamp.firstapp")
 public class FirstAppApplication {
 
 	public static void main(String[] args) {
